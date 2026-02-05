1d0a29b16e1f4b685a2ec8709bf674c0a87cafbb
SL0rD <antoniopacilli.info@gmail.com>
2026-02-05 22:32:59 +0000

    Ulteriore modifica al file filetype.lua, implementando la funzione vim.schedule con funzione parametro che innesca la listchas per ciascun filetype

--- lua/commands/options/filetype.lua
+++ lua/commands/options/filetype.lua
@@ -9,7 +9,11 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.softtabstop = 4
         vim.opt_local.expandtab = false
 
-        vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+		vim.schedule(
+			function()
+				vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+			end
+		)
     end,
     desc = "Forza indentazione a 4 spazi per Lua",
 })
@@ -17,7 +21,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Sovrascrittura per l'estensione 'Mermaid'
 vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
     -- Il pattern 'mmd' fa scattare questo comando solo per i file .lua
-    pattern={"*.mmd"},
+    pattern="*.mmd",
     callback = function()
         vim.bo.filetype="mermaid" -- forza l'identazione visiva e digitale del tab sui file mermaid
         -- **vim.opt_local** assicura che l'impostazione sia solo per il buffer aperto
@@ -26,7 +30,11 @@ vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
         vim.opt_local.softtabstop = 4
         vim.opt_local.expandtab = false
 
-        vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+		vim.schedule(
+			function()
+				vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+			end
+		)
     end,
     desc = "Forza indentazione a 4 spazi per Mermaid",
 })
@@ -34,7 +42,7 @@ vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
 -- Sovrascrittura per l'estensione 'Python'
 vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
     -- Il pattern 'mmd' fa scattare questo comando solo per i file .lua
-    pattern={"*.py"},
+    pattern="*.py",
     callback = function()
         vim.bo.filetype="python" -- forza l'identazione visiva e digitale del tab sui file mermaid
         -- **vim.opt_local** assicura che l'impostazione sia solo per il buffer aperto
@@ -43,15 +51,19 @@ vim.api.nvim_create_autocmd({"BufRead","BufNewFile"}, {
         vim.opt_local.softtabstop = 4
         vim.opt_local.expandtab = false
 
-        vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+		vim.schedule(
+			function()
+				vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+			end
+		)
     end,
     desc = "Forza indentazione a 4 spazi per Python",
 })
 
 -- Sovrascrittura per l'estensione 'Java'
 vim.api.nvim_create_autocmd("FileType", {
-    -- Il pattern 'mmd' fa scattare questo comando solo per i file .lua
-	pattern={"*.java"},
+    -- Il pattern 'java' fa scattare questo comando solo per i file .java
+	pattern="java",
     callback = function()
         vim.bo.filetype="java" -- forza l'identazione visiva e digitale del tab sui file mermaid
         -- **vim.opt_local** assicura che l'impostazione sia solo per il buffer aperto
@@ -60,7 +72,11 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.softtabstop = 4
         vim.opt_local.expandtab = false
 
-        vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+		vim.schedule(
+			function()
+				vim.opt_local.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+			end
+		)
     end,
     desc = "Forza indentazione a 4 spazi per Python",
 })
