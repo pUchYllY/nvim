26cf08d897c583c12d189802c777971e4f4db56b
SL0rD <antoniopacilli.info@gmail.com>
2026-02-11 15:55:35 +0000

    Aggiustato l'identazione del modulo 'auto-pairs.lua'

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -1,40 +1,40 @@
-	local a_p={
+local a_p={
 
-		'windwp/nvim-autopairs',
-		event = "InsertEnter",
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
 
-		opts = {
-			enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
-			disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
-			disable_in_macro = true, -- disable when recording or executing a macro
-			disable_in_visualblock = false, -- disable when insert after visual block mode
-			disable_in_replace_mode = true,
-			ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
-			enable_moveright = true,
-			enable_afterquote = true, -- add bracket pairs after quote
-			enable_check_bracket_line = true, --- check bracket in same line
-			enable_bracket_in_quote = true, --
-			enable_abbr = false, -- trigger abbreviation
-			break_undo = true, -- switch for basic rule break undo sequence
-			check_ts = false,
-			map_cr = true,
-			map_bs = true, -- map the <BS> key
-			map_c_h = false, -- Map the <C-h> key to delete a pair
-			map_c_w = false, -- map <c-w> to delete a pair if possible
-		},
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
+	opts = {
+		enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
+		disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+		disable_in_macro = true, -- disable when recording or executing a macro
+		disable_in_visualblock = false, -- disable when insert after visual block mode
+		disable_in_replace_mode = true,
+		ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+		enable_moveright = true,
+		enable_afterquote = true, -- add bracket pairs after quote
+		enable_check_bracket_line = true, --- check bracket in same line
+		enable_bracket_in_quote = true, --
+		enable_abbr = false, -- trigger abbreviation
+		break_undo = true, -- switch for basic rule break undo sequence
+		check_ts = false,
+		map_cr = true,
+		map_bs = true, -- map the <BS> key
+		map_c_h = false, -- Map the <C-h> key to delete a pair
+		map_c_w = false, -- map <c-w> to delete a pair if possible
+	},
+	-- use opts = {} for passing setup options
+	-- this is equivalent to setup({}) function
 
-		config=function(_,opts)
-			local npairs = require("nvim-autopairs") 
-			local Rule = require("nvim-autopairs.rule") 
+	config=function(_,opts)
+		local npairs = require("nvim-autopairs") 
+		local Rule = require("nvim-autopairs.rule") 
 
-			npairs.setup(opts)
+		npairs.setup(opts)
 
-			npairs.add_rules({
-				Rule("~","~","mermaid")
-					:with_pair(function() return true end)
-			})
+		npairs.add_rules({
+			Rule("~","~","mermaid")
+				:with_pair(function() return true end)
+		})
 	end,
 
 
