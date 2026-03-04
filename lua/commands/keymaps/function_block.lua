71911fbc57bae2179f4e7029be560da6baf3270b
SL0rD <antoniopacilli.info@gmail.com>
2026-03-04 08:47:01 +0000

    .

--- lua/commands/keymaps/function_block.lua
+++ lua/commands/keymaps/function_block.lua
@@ -1,33 +1,73 @@
 local M={}
 
 M.c_cppJavaVoid_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("void name_function(<Right>{<CR><Tab><CR><Up><Right>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local code=[[
+	void name_function(){
+		<cursor>
+	}
+	]]
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 M.c_cppjavaReturn_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("int name_function(<Right>{return 0;", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local code = [[
+	int name_function(){
+		<cursor>
+
+		return 0;
+	}
+	]]
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 M.cppJavaClassReturn_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("Class name_function(<Right>{return instance;<s-Left><s-Left><s-Left><CR><CR><CR><Tab><Up><Up><Tab>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local keys =[[
+	Class name_function(){
+		<cursor>
+
+		return instance;
+	}
+	]] 
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 M.lua_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("function(<Right>end<S-Left><CR><CR><Up><Tab>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local code = [[
+	function()
+		<cursor>
+	end
+	]]
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 M.pythonReturn_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("(<Right>->Class:<CR><CR><CR><Tab>return instance<Up><Up><Tab>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local code =[[
+	def name_function()->Class_name:
+		<cursor>
+		return instance
+	]] 
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 M.pythonPass_function_block=function()
-	local keys = vim.api.nvim_replace_termcodes("(<Right>:pass<S-Left><CR><CR><Up><Tab>", true, false, true)
-	vim.api.nvim_feedkeys(keys, "m", false)
+	local code =[[
+	def name_function()->None:
+		<cursor>
+		pass
+	]] 
+
+	code=code:gsub("<cursor>","")
+	return vim.api.nvim_replace_code_termcodes(code,true,false,true)
 end
 
 return M
