41da3f8192284d77f335de66aaf307a70737f2dc
SL0rD <antoniopacilli.info@gmail.com>
2026-07-11 11:27:29 +0000

    Restarting Nvim Configuration Project

--- init.lua
+++
@@ -1,8 +0,0 @@
--- aggiungi mason/bin al PATH
-vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
-
-require("commands.options")
-require("commands.keymaps")
-
-require("config.lazy")
-require("filetype")
